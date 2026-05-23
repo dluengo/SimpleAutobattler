d9cd2b7ae583f7d6d2586c4cd078a6c4232382e0
David "Ole" Luengo <olelen@gmail.com>
2026-05-23 12:49:30 +0100

    Cleaned up HPUI

--- Assets/Scripts/UI/HPUI.cs
+++ Assets/Scripts/UI/HPUI.cs
@@ -6,11 +6,7 @@ using System.Collections.Generic;
 public class HPUI : StatUIBase
 {
     // --- Members ---
-    //[SerializeField] HPStat hpStat;
-    //[SerializeField] Sprite emptyHeartSprite;
-    //[SerializeField] Sprite fullHeartSprite;
-    //[SerializeField] GridLayoutGroup heartSlots;
-    [Header("--- Stat UI Settings ---")]
+    [Header("--- HP UI Settings ---")]
     [SerializeField] Slider hpSlider;
     [SerializeField] TextMeshProUGUI hpText;
 
@@ -31,34 +27,11 @@ public class HPUI : StatUIBase
         base.Start();
 
         UpdateUI();
-
-
-        //// Populate the heartImages list and set the initial heart sprites.
-        //for (int i = 0; i < heartSlots.transform.childCount; i++) {
-        //    Image heartImage = heartSlots.transform.GetChild(i).GetComponent<Image>();
-        //    //heartImages.Add(heartImage);
-        //    heartImage.sprite = fullHeartSprite;
-        //}
     }
 
     protected override void UpdateUI()
     {
-        //float current = Mathf.Clamp(
-        //    hpStat.currentValue,
-        //    hpStat.minValue,
-        //    heartSlots.transform.childCount);
-
-        //// Update the transparency of the heart images based on the current HP value.
-        //for (int i = 0; i < heartSlots.transform.childCount; i++)
-        //{
-        //    Image heartImage = heartSlots.transform.GetChild(i).GetComponent<Image>();
-        //    Color color = heartImage.color;
-
-        //    color.a = (i < current) ? 1f : 0f;
-        //    heartImage.color = color;
-        //}
-
-        hpSlider.value = hpStat.currentValue / hpStat.maxValue;
+        hpSlider.value = (float)hpStat.currentValue / hpStat.maxValue;
         hpText.text = $"{hpStat.currentValue} / {hpStat.maxValue}";
     }
 }
